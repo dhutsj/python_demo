32ef04657d2bd15d1bb307e83a790b25636e18bd
dhutsj <shuangjie.tian@elsevier.com>
2023-02-04 14:40:30 +0800

    use session

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 from mysqlite import DBManager
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 SPORT = [
     "Basketball",
