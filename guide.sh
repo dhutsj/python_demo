12399d935d85acadcadbef88d716a0606ebd7d01
dhutsj <shuangjie.tian@elsevier.com>
2023-02-23 14:15:19 +0800

    user guide

--- guide.sh
+++ guide.sh
@@ -10,6 +10,8 @@ SELECT * FROM reg;
 UPDATE reg SET sport = "Football" WHERE name = "TSJ";
 DELETE FROM reg WHERE name = "TSJ";
 
-
+# https://opentelemetry.io/docs/instrumentation/python/exporters/
 flask run
 opentelemetry-instrument --traces_exporter console --metrics_exporter console flask run
+opentelemetry-instrument flask run
+# http://localhost:16686 jaeger
