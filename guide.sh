15dc61df466b4f8f64eb4e19213d7a6f22c7b075
dhutsj <shuangjie.tian@elsevier.com>
2023-02-27 15:41:31 +0800

    update guide

--- guide.sh
+++ guide.sh
@@ -15,3 +15,7 @@ flask run
 opentelemetry-instrument --traces_exporter console --metrics_exporter console flask run
 opentelemetry-instrument flask run
 # http://localhost:16686 jaeger
+
+#signoz
+# https://signoz.io/docs/instrumentation/flask/
+OTEL_RESOURCE_ATTRIBUTES=service.name=demoapp OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318"  opentelemetry-instrument --traces_exporter otlp_proto_http --metrics_exporter otlp_proto_http flask run
